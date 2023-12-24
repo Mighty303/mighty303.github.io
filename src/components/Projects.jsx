e80a3949aff067978a43fbefe1e6c9882f167e05
Mighty303 <martinwong303@gmail.com>
2023-12-24 01:25:23 -0800

    new card changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,27 +34,29 @@ const projects = [
 
 const Projects = () => {
     return (
-        <section id='projects' className='text-white flex flex-col items-start min-h-screen bg-black pt-12'>
+        <section id='projects' className='text-white flex flex-col items-start min-h-screen 3xl:min-h-min bg-black pt-12'>
             <h1 className=' font-extralight mx-12 md:mx-24 text-4xl py-12'>PROJECTS</h1>
-            <div className='flex flex-col items-center mx-12 md:mx-24 py-12'>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-12 sm:w-[50%] md:w-[60%]'>
+            <div className='flex justify-center mx-12 md:mx-24 py-12'>
+                <div className='grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-4 items-center justify-center gap-12 sm:w-[50%] md:w-[60%] 3xl:w-[80%]'>
                     {
                         projects.map((project, index) => {
                             return (
-                                <div key={index} id={index} className='group flex flex-col gap-4 bg-gray-900 rounded-2xl transition-none ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-700'>
-                                    <img src={project.thumbnail} className='w-full rounded-2xl' alt={`${projects.name}`} />
+                                <div key={index} id={index} className='group flex flex-col gap-6 bg-gray-900 rounded-2xl transition-all ease-in-out hover:-translate-y-2 hover:scale-100 duration-300 hover:shadow-3xl'>
+                                    <a href={project.github} rel='noopener noreferrrer' target='_blank'>
+                                        <img src={project.thumbnail} className='w-full rounded-t-2xl active:opacity-80' alt={`${projects.name}`} />
+                                    </a>
                                     <div className='flex justify-between items-center px-4'>
-                                        <h2 className='text-blue-200 font-bold text-2xl group-hover:text-blue-400'>{project.name}</h2>
+                                        <h2 className='text-white font-bold text-2xl group-hover:text-blue-400'>{project.name}</h2>
                                         <div className='flex gap-2 justify-center items-center'>
                                             {project.link && <a href={project.link} rel='noreferrrer noopener' target='_blank'><img src='images/icons/link.png' alt='link icon' className='w-6 h-6 hover:opacity-50' /></a>}
                                             <a href={project.github} rel='noreferrrer noopener' target='_blank'><img src='images/icons/github.png' alt='github icon' className='w-6 h-6 hover:opacity-50' /></a>
                                         </div>
                                     </div>
-                                    <p className='text-gray-300 px-4'>{project.description}</p>
+                                    <p className='text-gray-300 px-4 3xl:h-12'>{project.description}</p>
                                     <ul className='flex gap-2 px-4 pb-4'>
                                         {project.tech.map((tech, index) => {
                                             return (
-                                                <li key={index} className='text-blue-400'>{tech}</li>
+                                                <li key={index} className='text-blue-400 font-semibold'>{tech}</li>
                                             )
                                         })}
                                     </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ export default {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '2045px',
+        // => @media (min-width: 1280px) { ... }
+      },
+      boxShadow: {
+        // '3xl': '0 35px 35px rgba(100, 100, 100, 0.25)',
+        '3xl': '0 0 80px 5px rgba(90, 90, 90, 0.3)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
+    }
   },
   plugins: [require("daisyui")],
 }
