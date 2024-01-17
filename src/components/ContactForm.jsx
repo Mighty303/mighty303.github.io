bd63c81bf04746366bdbf08c603147ab8ee1c950
Mighty303 <martinwong303@gmail.com>
2024-01-16 23:07:36 -0800

    added form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,139 @@
+import { useRef, useState, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
+import { useForm } from 'react-hook-form';
+
+const ContactForm = () => {
+
+    const form = useRef();
+    const [submitState, setSubmitState] = useState(null);
+    const { 
+        register, 
+        handleSubmit, 
+        reset,
+        formState,
+        formState: { errors, isSubmitSuccessful } 
+    } = useForm();
+
+    useEffect(() => {
+      if (formState.isSubmitSuccessful) {
+        reset()
+      }
+    }, [formState, reset])
+
+    const sendEmail = async (e) => {
+      try {
+        const result = await emailjs.sendForm('service_sa565wk', 'template_zrazbxb', form.current, '2fTNM90hgIdKMWfLm');
+        console.log(result.text);
+        setSubmitState('success');
+      } catch (error) {
+        console.error(error);
+        setSubmitState('error');
+      }
+  
+      setTimeout(() => {
+        setSubmitState(null);
+      }, 3000);
+    };
+
+    return (
+      <div className='flex justify-center h-auto md:h-auto overflow-hidden rounded-lg'>
+        <form
+          className='p-8 border rounded-xl flex flex-col w-full md:w-1/3 gap-6'
+          ref={form}
+          onSubmit={handleSubmit(sendEmail)}
+        >
+            <div className='flex flex-col gap-1'>
+                <label>
+                    Name <span className='text-red-500 font-bold'>*</span>
+                </label>
+                <input
+                    type='text'
+                    {...register('name', {
+                        required: 'This is required',
+                        minLength: {
+                        value: 2,
+                        message: 'Name must be at least 2 characters',
+                        },
+                    })}
+                    placeholder='Name'
+                    className={`border border-${errors.name?.message ? 'rose-500' : 'gray-300' }  rounded-md p-3 bg-white text-black placeholder-gray-500`}
+                />
+                <p className='text-red-500 font-semibold'>
+                    {errors.name?.message}
+                </p>
+            </div>
+            <div className='flex flex-col gap-1'>
+                <label>
+                    Email <span className='text-red-500 font-bold'>*</span>
+                </label>
+                <input
+                    type='email'
+                    {...register('client_email', {
+                    required: 'This is required',
+                    pattern: {
+                        value: /^([A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,})$/i,
+                        message: 'Invalid email address',
+                    },
+                    })}
+                    placeholder='Email'
+                    className={`border border-${errors.client_email?.message ? 'rose-500' : 'gray-300' } rounded-md p-3 bg-white text-black placeholder-gray-500`}
+                />
+                <p className='text-red-500 font-semibold'>
+                    {errors.client_email?.message}
+                </p>
+            </div>
+            <div className='flex flex-col gap-1'>
+                <label>
+                    Phone Number
+                </label>
+                <input
+                    type='tel'
+                    {...register('client_phone', {
+                    pattern: {
+                        value: /^(?:\+?1\s?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/,
+                        message: 'Invalid phone number',
+                    },
+                    })}
+                    placeholder='+1 (604) 000-0000'
+                    className={`border border-${errors.client_phone?.message ? 'rose-500' : 'gray-300' } rounded-md p-3 bg-white text-black placeholder-gray-500`}
+                />
+                <p className='text-red-500 font-semibold'>
+                    {errors.client_phone?.message}
+                </p>
+            </div>
+            <div className='flex flex-col gap-1'>
+                <label>Message</label>
+                <textarea
+                    {...register('client_message')}
+                    className='border border-gray-300 rounded-md px-2 pb-10 md:px-4 md:pt-4 md:pb-20 resize-none bg-white text-black placeholder-gray-500'
+                    placeholder='Write your message here...'
+                />
+                <p className='text-red-500 font-semibold'>
+                    {errors.client_message?.message}
+                </p>
+            </div>
+            <button className='flex items-center gap-2 mx-auto bg-blue-600 text-white px-4 py-3 rounded-lg hover:bg-blue-500 active:opacity-80 duration-300 '>
+                <img src='images/icons/email.png' alt='email' className='h-5 w-5' />
+                Send Message
+            </button>
+        </form>
+
+        {submitState === 'success' && (
+          <div role='alert' className='toast'>
+            <div className='alert alert-success text-white'>
+              <span>Message Sent Successfully</span>
+            </div>
+          </div>
+        )}
+        {submitState === 'error' && (
+          <div role='alert' className='toast'>
+            <div className='alert alert-error text-white'>
+              <span>Error, please try again</span>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+}
+        
+export default ContactForm;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
+import ContactForm from "./ContactForm";
+
 const Footer = () => {
     return (
         <section id='footer' className='flex flex-col items-center bg-black text-white pb-12'>
             <div className='mx-12 md:mx-24 w-[80%]'>
                 <hr className='p-12 mt-12' />
                 <h1 className='font-extralight text-4xl mb-12 uppercase'>Contact</h1>
+                <ContactForm />
                 <div className='flex items-center justify-center gap-4'>
-                    <a href='mailto:martinwong303@gmail.com' rel='noopener noreferrrer' target='_blank'>
+
+                    {/* <a href='mailto:martinwong303@gmail.com' rel='noopener noreferrrer' target='_blank'>
                         <div className='h-14 w-32 md:w-40 rounded-md bg-white hover:bg-blue-500 ease-in-out duration-300 p-[3px]'>
                             <div className='flex h-full w-full items-center justify-center bg-black '>
                                 <img src='images/icons/email.png' alt='email' className='h-5 w-5' />
@@ -20,7 +24,7 @@ const Footer = () => {
                                 <p className='text-sm md:text-lg text-white p-1'>LinkedIn</p>
                             </div>
                         </div>
-                    </a>
+                    </a> */}
                 </div>
             </div>
         </section>
