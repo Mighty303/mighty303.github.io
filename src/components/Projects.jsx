e35b1d5bb6f14a93bce461d2f3dfed551ba0fee7
Mighty303 <martinwong303@gmail.com>
2024-01-17 20:13:32 -0800

    better mobile queries

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,19 @@
 const projects = [
     {
+        name: 'Broadway Smiles Redesign (WIP)',
+        thumbnail: 'images/BWS.png',
+        link: 'https://bws.avenahost.com/',
+        description: 'Redesigned and developed a dental clinic website to be more modern and improve load times.',
+        tech: ['React', 'Tailwind', 'NodeJS', 'MongoDB']
+    },
+    {
+        name: 'Avena Tech Redesign (WIP)',
+        thumbnail: 'images/avena.png',
+        link: 'https://avenatech.ca/',
+        description: 'Redesigned and developed company website to be more modern and user friendly.',
+        tech: ['React', 'Tailwind']
+    },
+    {
         name: 'Kanban Planner',
         thumbnail: 'images/kanban.png',
         github: 'https://github.com/Mighty303/Kanban-planner',
@@ -37,23 +51,23 @@ const Projects = () => {
         <section id='projects' className='text-white flex flex-col items-start min-h-screen 3xl:min-h-min bg-black pt-12'>
             <h1 className=' font-extralight mx-12 md:mx-24 text-4xl py-12'>PROJECTS</h1>
             <div className='flex justify-center mx-12 md:mx-24 py-12'>
-                <div className='grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-4 items-center justify-center gap-12 sm:w-[50%] md:w-[60%] 3xl:w-[80%]'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 items-center justify-center gap-12 sm:w-[50%] md:w-[100%] lg:w-[90%] 3xl:w-[80%]'>
                     {
                         projects.map((project, index) => {
                             return (
-                                <div key={index} id={index} className='group flex flex-col gap-6 bg-gray-900 rounded-2xl transition-all ease-out hover:-translate-y-2 hover:scale-100 duration-300 hover:shadow-3xl'>
-                                    <a href={project.github} rel='noopener noreferrrer' target='_blank'>
+                                <div key={index} id={index} className='group flex flex-col gap-6 h-full bg-gray-900 rounded-2xl transition-all ease-out hover:-translate-y-2 hover:scale-100 duration-300 hover:shadow-3xl'>
+                                    <a href={project.link || project.github} rel='noopener noreferrrer' target='_blank'>
                                         <img src={project.thumbnail} className='w-full rounded-t-2xl active:opacity-80' alt={`${projects.name}`} />
                                     </a>
                                     <div className='flex justify-between items-center px-4'>
                                         <h2 className='text-white font-bold text-2xl group-hover:text-blue-400'>{project.name}</h2>
                                         <div className='flex gap-2 justify-center items-center'>
                                             {project.link && <a href={project.link} rel='noreferrrer noopener' target='_blank'><img src='images/icons/link.png' alt='link icon' className='w-6 h-6 hover:opacity-50' /></a>}
-                                            <a href={project.github} rel='noreferrrer noopener' target='_blank'><img src='images/icons/github.png' alt='github icon' className='w-6 h-6 hover:opacity-50' /></a>
+                                            {project.github && <a href={project.github} rel='noreferrrer noopener' target='_blank'><img src='images/icons/github.png' alt='github icon' className='w-6 h-6 hover:opacity-50' /></a>}
                                         </div>
                                     </div>
                                     <p className='text-gray-300 px-4 3xl:h-12'>{project.description}</p>
-                                    <ul className='flex gap-2 px-4 pb-4'>
+                                    <ul className='flex flex-wrap gap-2 px-4 pb-4'>
                                         {project.tech.map((tech, index) => {
                                             return (
                                                 <li key={index} className='text-blue-400 font-semibold'>{tech}</li>
